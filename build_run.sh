a522e336bca4560680e5ced79f6aa9a39b14fada
mr360 <qd0097@gmail.com>
2023-12-10 06:14:55 +0000

    chore: easy rerun after changes

---
+++ build_run.sh
@@ -0,0 +1,3 @@
+# find . -name "*.dcm" -exec echo {} \; -exec dicom-decompress --transcode {} {} \;
+cp DCE_IDandPhaseSelect/DCE_IDandPhaseSelect.py ~/tmp/Slicer-5.6.0-linux-amd64/slicer.org/Extensions-32390/Breast_DCEMRI_FTV/lib/Slicer-5.6/qt-scripted-modules/DCE_IDandPhaseSelect.py 
+Slicer
